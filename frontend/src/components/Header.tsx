abb68d0d687742723de0324918ef3d8507d7836c
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-12-08 22:02:54 -0500

    Added an icon to the title, linked the title and icon to '/', and added in a tooltip for when the profile image is hovered over.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import Tooltip from "@mui/material/Tooltip";
 import { User } from "../dummyUser";
 
 const Header = () => {
@@ -17,24 +19,41 @@ const Header = () => {
         boxSizing: "border-box",
       }}
     >
-      <Typography sx={{ fontWeight: "bold" }}>Project Lifecycle Hub</Typography>
+      <Typography
+        sx={{
+          fontWeight: "bold",
+          textDecoration: "none",
+          color: "black",
+          display: "flex",
+          flexDirection: "row",
+          gap: 2,
+          alignItems: "center",
+        }}
+        component={"a"}
+        href="/"
+      >
+        <AccountTreeIcon />
+        Project Lifecycle Hub
+      </Typography>
       <Stack sx={{ flexDirection: "row", gap: 2, alignItems: "center" }}>
         <Typography sx={{ display: { xs: "none", sm: "block" } }}>
           Hello, {User.firstName}!
         </Typography>
-        <img
-          style={{
-            display: "block",
-            width: "35px",
-            height: "35px",
-            border: "solid rgba(215, 215, 215, 0.6)",
-            borderRadius: "100px",
-          }}
-          src={
-            User.imgUrl ??
-            "https://i0.wp.com/e-quester.com/wp-content/uploads/2021/11/placeholder-image-person-jpg.jpg?fit=820%2C678&ssl=1"
-          }
-        />
+        <Tooltip title="Nothing here yet">
+          <img
+            style={{
+              display: "block",
+              width: "35px",
+              height: "35px",
+              border: "solid rgba(215, 215, 215, 0.6)",
+              borderRadius: "100px",
+            }}
+            src={
+              User.imgUrl ??
+              "https://i0.wp.com/e-quester.com/wp-content/uploads/2021/11/placeholder-image-person-jpg.jpg?fit=820%2C678&ssl=1"
+            }
+          />
+        </Tooltip>
       </Stack>
     </Stack>
   );
