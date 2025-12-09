289f5a8a6dad700f9471d22cd04a2d850e82f6cc
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-12-08 21:26:04 -0500

    Added empty pages to setup react-router-dom

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Stack>
+      <Typography>Home Page</Typography>
+    </Stack>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/ProjectDetails.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+const ProjectDetails = () => {
+  return (
+    <Stack>
+      <Typography>Project Details Page</Typography>
+    </Stack>
+  );
+};
+
+export default ProjectDetails;
