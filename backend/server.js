432b3a5ff5f55a9e29fbb39afdd0b77fa5f333a9
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-12-08 23:08:07 -0500

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const projectRoutes = require("./routes/projectRoutes");
 const stageRoutes = require("./routes/stageRoutes");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.ORIGIN || "http://localhost:5173",
+  })
+);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
