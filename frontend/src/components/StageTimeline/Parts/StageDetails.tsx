a00b3faba1f27ef30727b6e81f066290e81bbea7
joshuatsmith96 <joshuatsmith96@gmail.com>
2025-12-10 23:09:47 -0500

    Added data to the details section.

--- frontend/src/components/StageTimeline/Parts/StageDetails.tsx
+++ frontend/src/components/StageTimeline/Parts/StageDetails.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, TextField, Button } from "@mui/material";
 import type { StageDetailType, Stages } from "../../../types/DataTypes";
 
 type StageDetailsType = {
@@ -7,30 +7,85 @@ type StageDetailsType = {
 };
 
 const StageDetails = ({ stageDetails, stages }: StageDetailsType) => {
-  const currentStage: StageDetailType[] = stageDetails.filter(
+  const currentStage = stageDetails.find(
     (stage) => stage.project_stage_status === "Started"
   );
 
-  const stageName2 = () => {
-    const matchedStage = stages.find(
-      (s) => s.stage_id === currentStage[0].stage_id
-    );
-    return matchedStage?.stage_name;
-  };
+  if (!currentStage) {
+    return <Typography>No stage started yet.</Typography>;
+  }
+
+  const stageName = stages.find(
+    (s) => s.stage_id === currentStage.stage_id
+  )?.stage_name;
 
   return (
     <Stack
       sx={{
         border: "solid thin rgba(167, 167, 167, 1)",
-        height: "70vh",
         p: 2,
-        width: {
-          xs: "auto",
-          lg: "100%",
-        },
+        width: { xs: "auto", lg: "100%" },
+        gap: 3,
       }}
     >
-      <Typography sx={{ fontWeight: "bold" }}>{stageName2()} Stage</Typography>
+      <Typography sx={{ fontWeight: "bold" }}>{stageName} Stage</Typography>
+
+      <Stack>
+        <Typography sx={{ fontWeight: "bold", mb: 2 }}>Notes</Typography>
+        <TextField
+          multiline
+          minRows={10}
+          value={currentStage.project_stage_notes ?? ""}
+        />
+      </Stack>
+
+      <Stack>
+        <Typography sx={{ fontWeight: "bold", mb: 2 }}>
+          Attachements / Documents
+        </Typography>
+        <Stack
+          sx={{
+            border: "solid thin rgba(167, 167, 167, 1)",
+            alignItems: "center",
+            justifyContent: "center",
+            p: 3,
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>
+            No Attachments Yet
+          </Typography>
+          <Typography sx={{ color: "#2BAAFF" }}>Download</Typography>
+        </Stack>
+      </Stack>
+
+      <Stack
+        sx={{
+          flexDirection: { xs: "column", md: "row" },
+          justifyContent: "space-between",
+          gap: 2,
+        }}
+      >
+        <Button
+          variant="outlined"
+          sx={{
+            width: {
+              xs: "100%",
+              md: 200,
+            },
+            fontSize: 12,
+          }}
+        >
+          Save
+        </Button>
+        <Stack sx={{ flexDirection: { xs: "column", md: "row" }, gap: 2 }}>
+          <Button variant="contained" sx={{ fontSize: 12 }}>
+            Go Back to Previous Stage
+          </Button>
+          <Button variant="contained" sx={{ fontSize: 12 }} color="success">
+            Move to Next Stage
+          </Button>
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
